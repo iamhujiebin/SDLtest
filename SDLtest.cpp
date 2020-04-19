126cc7587389aaf8a5be4697056d0c4809cc4807
jiebin.hu <458249864@qq.com>
2020-04-19 13:03:10 +0800

    添加项目文件。

---
+++ SDLtest.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
+using namespace std;
+
+int SDL_main(int argc, char* argv[])
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+    else {
+        std::cout << "init success" << endl;
+    }
+    SDL_Quit();
+    return 0;
+}
